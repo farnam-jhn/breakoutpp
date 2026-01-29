16778916aeb68f1ba85a9965743063f7b82b4e18
Farnam-jhn <farnamjahangard@gmail.com>
2026-01-29 11:59:02 +0330

    Added Help menu

--- main.cpp
+++ main.cpp
@@ -24,6 +24,7 @@ struct Location{
 // Functions prototype
 
 void ballLocationFunction(Location loc, double slope, double hDistance);
+void helpMenu();
 
 // Main function
 
@@ -39,7 +40,7 @@ int main(){
             // TODO
             break;
             case '2':
-            // TODO
+                helpMenu();
             break;
             case '3':
             // TODO
@@ -68,4 +69,64 @@ void ballLocationFunction(Location loc, double slope, double hDistance){
 
     loc.y = (double)loc.x * slope + hDistance;
 
+}
+
+void helpMenu(){
+    clear();
+    std::cout << std::endl << std::endl
+              << "\033[34m           ╔══════════════════════════════════════════════════════════════════════╗      \033[0m" << std::endl
+              << "\033[34m           ║\033[0m"
+              << "                                                                      "
+              << "\033[34m║      \033[0m" << std::endl
+              << "\033[34m           ║\033[0m"
+              << "  The game consist of few elements :                                  "
+              << "\033[34m║      \033[0m" << std::endl
+              << "\033[34m           ║\033[0m"
+              << "  1. The ball, 2. Paddel, 3. Blocks                                   "
+              << "\033[34m║      \033[0m" << std::endl
+              << "\033[34m           ║\033[0m"
+              << "                                                                      "
+              << "\033[34m║      \033[0m" << std::endl
+              << "\033[34m           ║\033[0m"
+              << "  The ball starts moving in a line; whenever the ball hits the wall,  "
+              << "\033[34m║      \033[0m" << std::endl
+              << "\033[34m           ║\033[0m"
+              << "  a block or the paddel it reflects like a beam and whenever it hits  "
+              << "\033[34m║      \033[0m" << std::endl
+              << "\033[34m           ║\033[0m"
+              << "  a block, the block gets destroyed.                                  "
+              << "\033[34m║      \033[0m" << std::endl
+              << "\033[34m           ║\033[0m"
+              << "                                                                      "
+              << "\033[34m║      \033[0m" << std::endl
+              << "\033[34m           ║\033[0m"
+              << "  The goal is to destroy all of the blocks                            "
+              << "\033[34m║      \033[0m" << std::endl
+              << "\033[34m           ║\033[0m"
+              << "                                                                      "
+              << "\033[34m║      \033[0m" << std::endl
+              << "\033[34m           ║\033[0m"
+              << "  If your ball fall down and don't touch the paddel you lose a        "
+              << "\033[34m║      \033[0m" << std::endl
+              << "\033[34m           ║\033[0m"
+              << "  heart; you have 3 hearts, if you lose them all you lose.            "
+              << "\033[34m║      \033[0m" << std::endl
+              << "\033[34m           ║\033[0m"
+              << "                                                                      "
+              << "\033[34m║      \033[0m" << std::endl
+              << "\033[34m           ║\033[0m"
+              << "  You can move the paddel using \"A\" & \"D\" to move left & right.       "
+              << "\033[34m║      \033[0m" << std::endl
+              << "\033[34m           ║\033[0m"
+              << "                                                                      "
+              << "\033[34m║      \033[0m" << std::endl
+              << "\033[34m           ║\033[0m"
+              << "  Press any button to exit.                                           "
+              << "\033[34m║      \033[0m" << std::endl
+              << "\033[34m           ║\033[0m"
+              << "                                                                      "
+              << "\033[34m║      \033[0m" << std::endl
+              << "\033[34m           ╚══════════════════════════════════════════════════════════════════════╝      \033[0m" << std::endl;
+
+    getch();
 }
