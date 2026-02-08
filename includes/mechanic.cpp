8b83a8da6d3d66b64af3a9a8999ddc779fed4bd4
Matin-Ardestani <m.matin.ardestani@gmail.com>
2026-02-09 02:39:17 +0330

    added basic machanics of brick collision

--- includes/mechanic.cpp
+++ includes/mechanic.cpp
@@ -25,73 +25,118 @@ void ballmover(Ball &ball){
     int tempNewX = ball.loc.x + ball.v.vX;
     int tempNewY = ball.loc.y + ball.v.vY;
 
-    int brick_tempNewX, brick_tempNewY = tempNewY;
-    for(int i = tempNewX; i > 0; i--){
-        if(i % 5 == 0){
-            brick_tempNewX = i;
-            break;
+    // Checking collision for current position
+    for(int i = 0; i < 36; i++){
+        if(bricks_idx[i] != -1 && bricks_idy[i] != -1){
+            int brick_row = bricks_idx[i];
+            int brick_col_start = bricks_idy[i];
+            
+            // Check if ball is stuck in a brick
+            if(ball.loc.y == brick_row && 
+               ball.loc.x >= brick_col_start && 
+               ball.loc.x <= brick_col_start + 4){
+                // reverse both velocities
+                ball.v.vX = -ball.v.vX;
+                ball.v.vY = -ball.v.vY;
+                tempNewX = ball.loc.x + ball.v.vX;
+                tempNewY = ball.loc.y + ball.v.vY;
+                // delete brick and add score
+                player.score += 1000;
+                bricks_idx[i] = -1;
+                bricks_idy[i] = -1;
+                
+                goto skip_normal_collision;  // Skip rest of collision checks
+            }
         }
     }
-    int brick_index = (brick_tempNewY - 1) * 12 + (brick_tempNewX / 5) - 2;
-    if(isBrick(brick_tempNewY, brick_tempNewX)){
-        if(ball.v.vX > 0){ // heading right
-            if(bricks_idx[brick_index - 1] == -1 && brick_tempNewX == tempNewX){
-                ball.v.vX = -ball.v.vX;
+    
+    // Checking brick collison for new position
+    for(int i = 0; i < 36; i++){
+        if(bricks_idx[i] != -1 && bricks_idy[i] != -1){
+            int brick_row = bricks_idx[i];
+            int brick_col_start = bricks_idy[i];
+            int brick_col_end = brick_col_start + 4;
+            
+            // checking if there is a brick there, Works like (isBrick( , )) function
+            if(tempNewY == brick_row && 
+               tempNewX >= brick_col_start && 
+               tempNewX <= brick_col_end){
+                
+                bool bounceX = false;
+                bool bounceY = false;
+                
+                // X direction
+                if(ball.v.vX > 0 && ball.loc.x < brick_col_start){ // heading right
+                    bounceX = true;
+                }
+                else if(ball.v.vX < 0 && ball.loc.x > brick_col_end){ // heading left
+                    bounceX = true;
+                }
+                
+                // Y direction
+                if(ball.v.vY > 0 && ball.loc.y < brick_row){ // heading down
+                    bounceY = true;
+                }
+                else if(ball.v.vY < 0 && ball.loc.y > brick_row){ // heading up
+                    bounceY = true;
+                }
+                
+                if(bounceX && bounceY){ // Corner hit
+                    ball.v.vX = -ball.v.vX;
+                    ball.v.vY = -ball.v.vY;
+                }
+                else if(bounceX){ // Side hit
+                    ball.v.vX = -ball.v.vX;
+                }
+                else { // Top/bottom hit
+                    ball.v.vY = -ball.v.vY;
+                }
+                
+                // Recalculate position after bounce
                 tempNewX = ball.loc.x + ball.v.vX;
-            }
-            else{
-                ball.v.vY = -ball.v.vY;
                 tempNewY = ball.loc.y + ball.v.vY;
+                
+                // delete brick and add score
+                player.score += 1000;
+                bricks_idx[i] = -1;
+                bricks_idy[i] = -1;
+                
+                break;  // Only handle one brick per frame
             }
         }
-        else{ // heading left
-            if(bricks_idx[brick_index + 1] == -1 && brick_tempNewX + 4 == tempNewX){
-                ball.v.vX = -ball.v.vX;
-                tempNewX = ball.loc.x + ball.v.vX;
-            }
-            else{
-                ball.v.vY = -ball.v.vY;
-                tempNewY = ball.loc.y + ball.v.vY;
-            }
-        }
-
-        // deleting the brick and adding the score
-        board[brick_tempNewY][brick_tempNewX] = " ";
-        player.score += 1000;
-        bricks_idx[brick_index] = -1;
-        bricks_idy[brick_index] = -1;
     }
-    else{
-        if (tempNewX <= 0 || tempNewX >= board_lenght - 1){
-            ball.v.vX = -ball.v.vX;
-            tempNewX = ball.loc.x + ball.v.vX;
-        }
+    
+    skip_normal_collision:
+    
+    // Wall collision
+    if (tempNewX <= 0 || tempNewX >= board_lenght - 1){
+        ball.v.vX = -ball.v.vX;
+        tempNewX = ball.loc.x + ball.v.vX;
+    }
+    if (tempNewY <= 0 || tempNewY >= board_width - 1){
+        ball.v.vY = -ball.v.vY;
+        tempNewY = ball.loc.y + ball.v.vY;
+    }
 
-        if (tempNewY <= 0 || tempNewY >= board_width - 1){
-            ball.v.vY = -ball.v.vY;
-            tempNewY = ball.loc.y + ball.v.vY;
-        }
+    // Paddle collision
+    if (tempNewY == board_width - 2) {
+        if (tempNewX >= paddle.start_loc.x && tempNewX < paddle.start_loc.x + 10) {
 
-        if (tempNewY == board_width - 2) {
-            // Check if the ball's X position is inside the paddle (width 10)
-            if (tempNewX >= paddle.start_loc.x && tempNewX < paddle.start_loc.x + 10) {
+            int distanceFromPS = tempNewX - paddle.start_loc.x;
 
-                int distanceFromPS = tempNewX - paddle.start_loc.x;
+            ball.v.vY = -1; // Force ball to move UP
 
-                ball.v.vY = -1; // Force ball to move UP
+            int velocityMagnitude[10] = {3,2,2,1,1,1,1,2,2,3}; // an array that gives any x axis distance between the ball and paddle start a velocity
 
-                int velocityMagnitude[10] = {3,2,2,1,1,1,1,2,2,3}; // an array that gives any x axis distance between the ball and paddle start a velocity
-
-                if (ball.v.vX > 0){
-                    ball.v.vX = velocityMagnitude[distanceFromPS];
-                }
-                else {
-                    ball.v.vX = -velocityMagnitude[distanceFromPS];
-                }
-
-                tempNewY = ball.loc.y + ball.v.vY; // Update next Y immediately
-
+            if (ball.v.vX > 0){
+                ball.v.vX = velocityMagnitude[distanceFromPS];
             }
+            else {
+                ball.v.vX = -velocityMagnitude[distanceFromPS];
+            }
+
+            tempNewY = ball.loc.y + ball.v.vY; // Update next Y immediately
+
         }
     }
 
